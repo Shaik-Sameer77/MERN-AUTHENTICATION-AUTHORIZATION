e74a2b2a8e8360bafbdc3790c9308aa680455d63
Shaik Sameer <130442189+Shaik-Sameer77@users.noreply.github.com>
2025-09-26 14:39:11 +0530

    removed app lister for vercel deployment

--- backend/index.js
+++ backend/index.js
@@ -44,8 +44,10 @@ app.use(
 //routes
 app.use("/api/user", userRouter);
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`server is running at port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`server is running at port ${PORT}`);
+// });
+
+export default app
