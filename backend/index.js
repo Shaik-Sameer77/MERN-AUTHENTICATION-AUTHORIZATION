567c9c92939956d34ff4a41c8dac2d9b1484c587
Shaik Sameer <130442189+Shaik-Sameer77@users.noreply.github.com>
2025-09-26 16:04:06 +0530

    backend removed app listener

--- backend/index.js
+++ backend/index.js
@@ -44,7 +44,7 @@ app.use(
 //routes
 app.use("/api/user", userRouter);
 
-// const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 // app.listen(PORT, () => {
 //   console.log(`server is running at port ${PORT}`);
