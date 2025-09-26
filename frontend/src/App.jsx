ff2165b4915912cdaad47018872c0d9b26b124c5
Shaik Sameer <130442189+Shaik-Sameer77@users.noreply.github.com>
2025-09-26 10:57:00 +0530

    frontend App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/home/Home.jsx";
-import Login from "./pages/Login/Login.jsx";
+import Login from "./pages/login/Login.jsx";
 import Register from "./pages/register/Register.jsx";
 import { ToastContainer } from "react-toastify";
 import VerifyOtp from "./pages/verifyOtp/VerifyOtp.jsx";
